d5cb3fd970f1c5ec944fbc432a4a0385ea9cd78a
necip <43618473+ncpy@users.noreply.github.com>
2023-06-30 07:08:46 +0200

    fix: getStreamById was missing

--- frontend/src/pages/Page2.js
+++ frontend/src/pages/Page2.js
@@ -2,32 +2,11 @@ import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchStreams } from '../redux/streamSlice';
 import { useParams } from 'react-router-dom';
+import StreamerRecord from '../components/StreamerRecord';
 
 const Page2 = () => {
-  const dispatch = useDispatch()
-  const { streamID } = useParams()
-
-  const streamer = useSelector(state => state.stream.streamList).find(streamer => streamer.id === streamID)
-
-  useEffect(() => {
-    dispatch(fetchStreams())
-  }, [dispatch]);
-
-  if (!streamer) {
-    return <div>Loading...</div>;
-  }
-
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <div className="max-w-sm mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-        <img src={streamer.image} alt={streamer.name} />
-        <div className="p-4">
-          <h2 className="text-xl font-bold mb-2">{streamer.name}</h2>
-          <p className="text-gray-700 text-base mb-2">{streamer.description}</p>
-          <p className="text-gray-500 text-sm">Platform: {streamer.platform}</p>
-        </div>
-      </div>
-    </div>
+    <StreamerRecord />
   )
 }
 
